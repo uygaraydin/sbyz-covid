11bfaf8ad9cec458367199dce990d0c27e2580c3
EmrahGurlek <e.gurlek@gmail.com>
2021-05-03 17:59:36 +0300

    Hay Allah, silindi.

--- Untitled.R
+++ Untitled.R
@@ -1,7 +1,2 @@
 data = read.csv(file = "corona_tested_individuals_ver_006.english.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 summary(data)
-<<<<<<< HEAD
-
-## uygar ekledi
-=======
->>>>>>> 5122712b0d3f4094a36b7a298f25785987ace3e7
