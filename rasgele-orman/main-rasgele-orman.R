9201e63f2fd267a95930ec621ed24bb7effb9f08
Uygar <iletisim@uygaraydin.com>
2021-05-15 19:39:21 +0300

    rasgele orman algoritması için, veri seti train ve test olmak üzere ikiye ayrıldı

--- rasgele-orman/main-rasgele-orman.R
+++ rasgele-orman/main-rasgele-orman.R
@@ -1,2 +1,10 @@
 cleanedDataFile = read.csv(file = "./veri-on-isleme/temizlenmis-veri-seti.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 summary(cleanedDataFile)
+
+library(caret)
+set.seed(1)
+trainIndex=createDataPartition(cleanedDataFile$corona_result, p=0.7, list = FALSE)
+train=cleanedDataFile[trainIndex, ]
+test=cleanedDataFile[-trainIndex, ]
+
+library(randomForest)
