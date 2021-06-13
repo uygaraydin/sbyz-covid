3477d0a3d468a8d317de9102d6e850372cdc5354
nadamisk <nada.misk@gmail.com>
2021-06-13 21:48:34 +0300

    YSA son düzeltme

--- algoritmalar/yapay-sinir-aglari/main-yapay-sinir-aglari.R
+++ algoritmalar/yapay-sinir-aglari/main-yapay-sinir-aglari.R
@@ -1,21 +1,21 @@
 # train = read.csv(file = "./veri-on-isleme/genel_temizlik_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/genel_temizlik_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
-
+# 
 # train = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/random_dengeleme_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/random_dengeleme_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
-
+# 
 # train = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/dengesiz_veri_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/dengesiz_veri_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
-
+# 
 # train = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/unique_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/1-1_na-doldurma/unique_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 
-train = read.csv(file = "./veri-on-isleme/1-2_na-silme/random_dengeleme_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
-test = read.csv(file = "./veri-on-isleme/1-2_na-silme/random_dengeleme_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
+# train = read.csv(file = "./veri-on-isleme/1-2_na-silme/random_dengeleme_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
+# test = read.csv(file = "./veri-on-isleme/1-2_na-silme/random_dengeleme_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 
 # train = read.csv(file = "./veri-on-isleme/1-2_na-silme/dengesiz_veri_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/1-2_na-silme/dengesiz_veri_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
-
+# 
 # train = read.csv(file = "./veri-on-isleme/1-2_na-silme/unique_egitim.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 # test = read.csv(file = "./veri-on-isleme/1-2_na-silme/unique_test.csv", header = T, sep = ",", dec = ".", stringsAsFactors = T)
 
@@ -49,25 +49,6 @@ test$corona_result <-  ifelse(test$corona_result == "positive",1,0)
 
 
 
-
-# train$test_indication = as.double(train$test_indication)
-# 
-# summary(train)
-# test$test_indication = as.double(test$test_indication)
-# summary(test)
-
-
-
-# m <- model.matrix( 
-#   ~ corona_result+ cough+fever+sore_throat +shortness_of_breath+head_ache+age_60_and_above+gender+test_indication, 
-#   data = train 
-# )
-# 
-# testttt <- model.matrix( 
-#   ~ corona_result+ cough+fever+sore_throat +shortness_of_breath+head_ache+age_60_and_above+gender+test_indication, 
-#   data = test 
-# )
-
 train$corona_result =as.numeric(train$corona_result)
 train$age_60_and_above = as.numeric(train$age_60_and_above)
 train$gender = as.numeric(train$gender)
@@ -77,7 +58,6 @@ train$fever = as.numeric(train$fever)
 train$sore_throat = as.numeric(train$sore_throat)
 train$shortness_of_breath = as.numeric(train$shortness_of_breath)
 train$head_ache = as.numeric(train$head_ache)
-
 
 test$corona_result = as.numeric(test$corona_result)
 test$age_60_and_above = as.numeric(test$age_60_and_above)
@@ -89,61 +69,38 @@ test$sore_throat = as.numeric(test$sore_throat)
 test$shortness_of_breath = as.numeric(test$shortness_of_breath)
 test$head_ache = as.numeric(test$head_ache)
 
-#sigmoid function
-sigmoid = function(x) {
-  1 / (1 + exp(-x))
-}
-
-
 
 # colnames(m)
 library(neuralnet)
-(is.na(train))
-summary(train)
+
+
 nn = NULL
+set.seed(10)
 nn <- neuralnet(corona_result~cough+fever+sore_throat+shortness_of_breath+head_ache+age_60_and_above+gender+test_indication,
                 data=train,
                 linear.output = FALSE,
-                hidden = c(6),
-                err.fct = "ce",
-                learningrate = 0.01,
-                algorithm = "backprop",
-                act.fct = sigmoid,
+                hidden = 3,
+                act.fct = "logistic",
                 stepmax=1e6
 )
 
-plot(nn)
+#plot(nn)
+Predict=NULL
+set.seed(10)
 Predict=compute(nn,test[1:8])
-Predict$net.result
+#Predict$net.result
 summary(Predict)
 
-
 pred <- ifelse(Predict$net.result>0.5, 1, 0)
-pred
-# 
-# results <- data.frame(actual = test$corona_result, prediction = pred)
-# results
 
-library(caret)
+#library(caret)
 t <- table(pred,test$corona_result)  
-confusionMatrix(t, positive = "1")
+confusionMatrix(t, mode="everything", positive = "1")
+table(test$corona_result)
+table(pred)
 
-# tahminler <- NULL
-# tahminler <- neuralnet::compute (x =   covid.net,
-#                                  covariate =  testttt, rep = 1 )$net.result
-# 
-# table(tahminler)
-# kategorik_tahmin <- NULL
-# for (i in 1:nrow(tahminler)) {
-#   if ((tahminler[i,]) >= 0.35) kategorik_tahmin[i] <- "positive" else kategorik_tahmin[i] <- "negative"
-# }
-#covid.prediction <- predict(covid.net, test[1:8],type = "class")
-
-library(caret) 
-confusionMatrix(table(Predict, test$corona_result),mode="everything",positive ="positive")
-
-library(pROC)
-Logcl_rocAll=roc(test[,9]~Predict)
+#library(pROC)
+Logcl_rocAll=roc(test$corona_result~pred)
 print(Logcl_rocAll) #AUC
-plot(Logcl_rocAll,main="Dengele silinmis") 
+plot(Logcl_rocAll) 
 
